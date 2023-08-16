e3195c1ac9ff04c676877486e3c183961f128278
Anuranjan Srivastava <anuranjan.mail@gmail.com>
2023-08-16 11:32:43 +0530

    Added ability to login, get arbitrage from ws, make trades and view historical trades

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,26 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 // import { useSocketEvent } from "socket.io-react-hook";
 // import { Message } from "./types";
 
 import { io } from "socket.io-client";
-import { User } from "./types";
+import { Arbitrage, User } from "./types";
 import LoginForm from "./components/loginForm";
+import TradeCard from "./components/tradeCard";
+import HistTradeCard from "./components/histTradeCard";
 
-const URL = "http://localhost:5001";
+const ARBITRAGE_SERVICE_URL = "http://localhost:5001";
 
-const socket = io(URL);
+const socket = io(ARBITRAGE_SERVICE_URL);
+
+const TRADE_URL = "http://localhost:5002/api/trade";
 
 const App = () => {
-  const [input, setInput] = useState("");
-  const [messages, setMessages] = useState<string[]>([]);
+  const [threshold, setThreshold] = useState("123");
   const [isConnected, setIsConnected] = useState(socket.connected);
 
   const [user, setUser] = useState<User | null>(null);
+  const [arbitrageList, setArbitrageList] = useState<Arbitrage[]>([]);
+  const [tradeList, setTradeList] = useState<Arbitrage[]>([]);
 
   useEffect(() => {
     function onConnect() {
@@ -26,56 +31,142 @@ const App = () => {
       setIsConnected(false);
     }
 
-    function onMessageEvent(value: string) {
-      console.log(value);
-      setMessages((previous) => [...previous, value]);
+    function onArbitrageEvent(data: string) {
+      const arbitrageList = JSON.parse(data);
+      setArbitrageList(arbitrageList);
     }
 
     socket.on("connect", onConnect);
     socket.on("disconnect", onDisconnect);
-    socket.on("reverse", onMessageEvent);
+    socket.on("arbitrage", onArbitrageEvent);
 
     return () => {
       socket.off("connect", onConnect);
       socket.off("disconnect", onDisconnect);
-      socket.off("reverse", onMessageEvent);
+      socket.off("arbitrage", onArbitrageEvent);
     };
   }, []);
 
-  const sendMsg = () => {
-    socket.emit("reverse", input);
+  useEffect(() => {
+    if (threshold) {
+      socket.emit("threshold", threshold);
+    }
+  }, [threshold]);
+
+  const sendThreshold = () => {
+    socket.emit("threshold", threshold);
   };
 
   const handleLogin = (user: User) => {
-    console.log(user);
     setUser(user);
   };
+
+  const handleTrade = useCallback(
+    (arbitrage: Arbitrage) => {
+      if (user) {
+        fetch(TRADE_URL + "/create", {
+          method: "POST",
+          body: JSON.stringify(arbitrage),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            Authorization: user.token,
+          },
+        })
+          .then((response) => response.json())
+          .then(
+            (data) =>
+              data.result &&
+              setTradeList((prev) => [...prev, data.result as Arbitrage])
+          )
+          .catch((err) => console.error(err));
+      }
+    },
+    [user]
+  );
+
+  const fetchTrade = useCallback(() => {
+    if (user) {
+      fetch(TRADE_URL, {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          Authorization: user.token,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => data.result && setTradeList(data.result as Arbitrage[]))
+        .catch((err) => console.error(err));
+    }
+  }, [user]);
 
   if (!user) return <LoginForm onLogin={handleLogin} />;
 
   return (
-    <div className="flex gap-4 flex-col items-start">
-      <span>
-        Connection Status: {isConnected ? "Connected" : "Not Connected"}
-      </span>
+    <div className="w-full">
+      <div className="flex gap-4 flex-col items-start pt-16 max-w-6xl m-auto">
+        <span>
+          Connection Status: {isConnected ? "Connected" : "Not Connected"}
+        </span>
 
-      <div className="flex gap-4">
-        <input
-          className="max-w-lg border-2"
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button
-          className="bg-blue-600 rounded-md text-white p-2 px-4"
-          onClick={sendMsg}
-        >
-          Send
-        </button>
+        <div className="flex gap-4">
+          <div className="flex gap-4 items-center">
+            <span>Threshold: </span>
+            <input
+              className="max-w-lg border-2"
+              type="number"
+              value={threshold}
+              onChange={(e) => setThreshold(e.target.value)}
+            />
+            <button
+              className="bg-blue-600 rounded-md text-white p-2 px-4"
+              onClick={sendThreshold}
+            >
+              Set Threshold
+            </button>
+          </div>
+        </div>
+        <span>Available Trades</span>
+        <div className="p-2 px-4 flex items-center justify-between w-full">
+          <span>Symbol</span>
+          <span>Currency</span>
+          <span>Buy Exchange</span>
+          <span>Buy Price</span>
+          <span>Sell Exchange</span>
+          <span>Sell Price</span>
+          <span>Units</span>
+          <span>Profit</span>
+          <span>Trade</span>
+        </div>
+        <div className="flex flex-col gap-4 w-full max-h-80 overflow-auto">
+          {arbitrageList.map((arbitrage, idx) => (
+            <TradeCard key={idx} arbitrage={arbitrage} onTrade={handleTrade} />
+          ))}
+        </div>
+        <div className="flex justify-between items-center w-full">
+          <span>Historical Trades</span>
+          <button
+            className="bg-blue-600 rounded-md text-white p-2 px-4"
+            onClick={() => fetchTrade()}
+          >
+            Refresh Historical Trades
+          </button>
+        </div>
+        <div className="p-2 px-4 flex items-center justify-between w-full">
+          <span>Symbol</span>
+          <span>Currency</span>
+          <span>Buy Exchange</span>
+          <span>Buy Price</span>
+          <span>Sell Exchange</span>
+          <span>Sell Price</span>
+          <span>Units</span>
+          <span>Profit</span>
+        </div>
+        <div className="flex flex-col gap-4 w-full max-h-80 overflow-auto">
+          {tradeList.map((arbitrage, idx) => (
+            <HistTradeCard key={idx} arbitrage={arbitrage} />
+          ))}
+        </div>
       </div>
-      {messages.map((message) => (
-        <p>{message}</p>
-      ))}
     </div>
   );
 };
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,13 +1,25 @@
 export type Message = {
   result: string;
 };
+
 export type User = {
   id: string;
   name: string;
   username: string;
   token: string;
 };
+
 export type AuthPayload = {
   username: string;
   password: string;
+};
+
+export type Arbitrage = {
+  symbol: string;
+  currency: string;
+  buy_exchange: string;
+  sell_exchange: string;
+  buy_price: number;
+  sell_price: number;
+  units: number;
 };
